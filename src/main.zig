fe2eeb55c3544199631040af89ff44127cf37a34
Joshua Ibrom <joshuaibrom@gmail.com>
2023-04-28 09:24:33 -0500

    Wrote tests and the diffing works, albeit with a memory leak.

--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,5 @@ const zap = @import("zap");
 const diffing = @import("diffing.zig");
 
 pub fn main() !void {
-  const allocator = std.heap.page_allocator;
-
-  _ = try diffing.calculate_distance(allocator, "Foo", "Bar");
 }
 
