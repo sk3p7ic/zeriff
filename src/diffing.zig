5c857acf733a3fe731beefc4cec0ec4a4c1198e0
Joshua Ibrom <joshuaibrom@gmail.com>
2023-05-01 09:19:34 -0500

    Ran zig fmt

--- src/diffing.zig
+++ src/diffing.zig
@@ -2,191 +2,190 @@ const std = @import("std");
 const ArrayList = std.ArrayList;
 
 pub const DiffAction = enum {
-  ignore,
-  add,
-  remove,
+    ignore,
+    add,
+    remove,
 
-  pub fn to_string(self: DiffAction) []const u8 {
-    return switch (self) {
-      .ignore => " ",
-      .add => "+",
-      .remove => "-",
-    };
-  }
+    pub fn to_string(self: DiffAction) []const u8 {
+        return switch (self) {
+            .ignore => " ",
+            .add => "+",
+            .remove => "-",
+        };
+    }
 };
 
 pub const PatchAction = struct {
-  action: DiffAction,
-  location: usize,
-  line: []const u8,
+    action: DiffAction,
+    location: usize,
+    line: []const u8,
 
-  pub fn to_string(self: PatchAction, alloc: std.mem.Allocator) []u8 {
-    if (std.fmt.allocPrint(alloc, "{d} {s} {s}", .{ self.location, self.action.to_string(), self.line })) |s| {
-      return s;
-    } else |_| {
-      return "";
+    pub fn to_string(self: PatchAction, alloc: std.mem.Allocator) []u8 {
+        if (std.fmt.allocPrint(alloc, "{d} {s} {s}", .{ self.location, self.action.to_string(), self.line })) |s| {
+            return s;
+        } else |_| {
+            return "";
+        }
     }
-  }
 };
 
 // This algo is from https://github.com/tsoding/piff/blob/master/piff.py (thanks tsoding)
 pub fn calculate_distance(alloc: std.mem.Allocator, s1: [][]const u8, s2: [][]const u8) anyerror![]PatchAction {
-  // Get the number of lines for each collection of lines
-  const m1: usize = s1.len;
-  const m2: usize = s2.len;
+    // Get the number of lines for each collection of lines
+    const m1: usize = s1.len;
+    const m2: usize = s2.len;
 
-  // Init the ArrayLists for storing matrices of distances and actions
-  var distances = ArrayList(ArrayList(u16)).init(alloc);
-  defer distances.deinit();
-  var actions = ArrayList(ArrayList(DiffAction)).init(alloc);
-  defer actions.deinit();
+    // Init the ArrayLists for storing matrices of distances and actions
+    var distances = ArrayList(ArrayList(u16)).init(alloc);
+    defer distances.deinit();
+    var actions = ArrayList(ArrayList(DiffAction)).init(alloc);
+    defer actions.deinit();
 
-
-  // Create the inner rows of the matrices
-  var i: usize = 0;
-  while (i < m1 + 1): (i += 1) {
-    var dist_row = ArrayList(u16).init(alloc);
-    try dist_row.appendNTimes(0, m2);
-    var act_row = ArrayList(DiffAction).init(alloc);
-    try act_row.appendNTimes(.ignore, m2);
-    try distances.append(dist_row);
-    try actions.append(act_row);
-  }
-
-  // Set the upper left corner to base cases
-  distances.items[0].items[0] = 0;
-  actions.items[0].items[0] = .ignore;
-
-  { // Set the first row to all additions
-    var n2: u16 = 1;
-    while (n2 < m2): (n2 += 1) {
-      const n1 = comptime 0;
-      distances.items[n1].items[n2] = n2;
-      actions.items[n1].items[n2] = .add;
+    // Create the inner rows of the matrices
+    var i: usize = 0;
+    while (i < m1 + 1) : (i += 1) {
+        var dist_row = ArrayList(u16).init(alloc);
+        try dist_row.appendNTimes(0, m2);
+        var act_row = ArrayList(DiffAction).init(alloc);
+        try act_row.appendNTimes(.ignore, m2);
+        try distances.append(dist_row);
+        try actions.append(act_row);
     }
-  }
 
-  { // Set the first column to all removals
-    var n1: u16 = 1;
-    while (n1 < m1): (n1 += 1) {
-      const n2 = comptime 0;
-      distances.items[n1].items[n2] = n1;
-      actions.items[n1].items[n2] = .remove;
-    }
-  }
+    // Set the upper left corner to base cases
+    distances.items[0].items[0] = 0;
+    actions.items[0].items[0] = .ignore;
 
-  { // Calculate the distances and actions
-    var n1: u16 = 1;
-    while (n1 < m1): (n1 += 1) {
-      var n2: u16 = 1;
-      while (n2 < m2): (n2 += 1) {
-        // Lines are the same--ignore
-        if (lines_are_equal(s1[n1-1], s2[n2-1])) {
-          distances.items[n1].items[n2] = distances.items[n1-1].items[n2-1];
-          actions.items[n1].items[n2] = .ignore;
-          continue;
+    { // Set the first row to all additions
+        var n2: u16 = 1;
+        while (n2 < m2) : (n2 += 1) {
+            const n1 = comptime 0;
+            distances.items[n1].items[n2] = n2;
+            actions.items[n1].items[n2] = .add;
         }
+    }
 
-        const remove = distances.items[n1-1].items[n2];
-        const add = distances.items[n1].items[n2-1];
-
-        distances.items[n1].items[n2] = remove;
-        actions.items[n1].items[n2] = .remove;
-
-        if (distances.items[n1].items[n2] > add) {
-          distances.items[n1].items[n2] = add;
-          actions.items[n1].items[n2] = .add;
+    { // Set the first column to all removals
+        var n1: u16 = 1;
+        while (n1 < m1) : (n1 += 1) {
+            const n2 = comptime 0;
+            distances.items[n1].items[n2] = n1;
+            actions.items[n1].items[n2] = .remove;
         }
-        
-        distances.items[n1].items[n2] += 1;
-      }
     }
-  }
 
-  // Generate the patch
-  var patch = ArrayList(PatchAction).init(alloc);
-  defer patch.deinit();
-  var n1 = m1 - 1;
-  var n2 = m2 - 1;
-  while (n1 > 0 or n2 > 0) {
-    const action = actions.items[n1].items[n2];
-    switch (action) {
-      .add => {
-        n2 -= 1;
-        try patch.append(.{ .action = .add, .location = n2, .line = s2[n2]});
-      },
-      .remove => {
-        n1 -= 1;
-        try patch.append(.{ .action = .remove, .location = n1, .line = s1[n1]});
-      },
-      .ignore => {
-        n1 -= 1;
-        n2 -= 1;
-      }
+    { // Calculate the distances and actions
+        var n1: u16 = 1;
+        while (n1 < m1) : (n1 += 1) {
+            var n2: u16 = 1;
+            while (n2 < m2) : (n2 += 1) {
+                // Lines are the same--ignore
+                if (lines_are_equal(s1[n1 - 1], s2[n2 - 1])) {
+                    distances.items[n1].items[n2] = distances.items[n1 - 1].items[n2 - 1];
+                    actions.items[n1].items[n2] = .ignore;
+                    continue;
+                }
+
+                const remove = distances.items[n1 - 1].items[n2];
+                const add = distances.items[n1].items[n2 - 1];
+
+                distances.items[n1].items[n2] = remove;
+                actions.items[n1].items[n2] = .remove;
+
+                if (distances.items[n1].items[n2] > add) {
+                    distances.items[n1].items[n2] = add;
+                    actions.items[n1].items[n2] = .add;
+                }
+
+                distances.items[n1].items[n2] += 1;
+            }
+        }
     }
-  }
-  // Deinit memory in the distances and actions ArrayLists
-  for (distances.items) |item| {
-    item.deinit();
-  }
-  for (actions.items) |item| {
-    item.deinit();
-  }
 
-  return patch.toOwnedSlice();
+    // Generate the patch
+    var patch = ArrayList(PatchAction).init(alloc);
+    defer patch.deinit();
+    var n1 = m1 - 1;
+    var n2 = m2 - 1;
+    while (n1 > 0 or n2 > 0) {
+        const action = actions.items[n1].items[n2];
+        switch (action) {
+            .add => {
+                n2 -= 1;
+                try patch.append(.{ .action = .add, .location = n2, .line = s2[n2] });
+            },
+            .remove => {
+                n1 -= 1;
+                try patch.append(.{ .action = .remove, .location = n1, .line = s1[n1] });
+            },
+            .ignore => {
+                n1 -= 1;
+                n2 -= 1;
+            },
+        }
+    }
+    // Deinit memory in the distances and actions ArrayLists
+    for (distances.items) |item| {
+        item.deinit();
+    }
+    for (actions.items) |item| {
+        item.deinit();
+    }
+
+    return patch.toOwnedSlice();
 }
 
 fn lines_are_equal(line1: []const u8, line2: []const u8) bool {
-  if (line1.len != line2.len) {
-    return false;
-  }
-
-  var i: usize = 0;
-  while (i < line1.len): (i+=1) {
-    if (line1[i] != line2[i]) {
-      return false;
+    if (line1.len != line2.len) {
+        return false;
     }
-  }
-  return true;
+
+    var i: usize = 0;
+    while (i < line1.len) : (i += 1) {
+        if (line1[i] != line2[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 test "proper diffing" {
-  const ts1 =
-    \\Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-    \\incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-    \\nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    \\Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
-    \\eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-    \\sunt in culpa qui officia deserunt mollit anim id est laborum.
-  ;
-  const ts2 =
-    \\Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-    \\incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-    \\nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    \\Duis aute irure in dolor reprehenderit in voluptate velit esse cillum dolore
-    \\eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-    \\sunt in culpa qui officia deserunt mollit anim id est laborum.
-  ;
+    const ts1 =
+        \\Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        \\incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+        \\nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        \\Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+        \\eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        \\sunt in culpa qui officia deserunt mollit anim id est laborum.
+    ;
+    const ts2 =
+        \\Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        \\incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+        \\nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        \\Duis aute irure in dolor reprehenderit in voluptate velit esse cillum dolore
+        \\eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        \\sunt in culpa qui officia deserunt mollit anim id est laborum.
+    ;
 
-  const alloc = std.testing.allocator;
-  var s1 = ArrayList([]const u8).init(alloc);
-  defer s1.deinit();
-  var s2 = ArrayList([]const u8).init(alloc);
-  defer s2.deinit();
+    const alloc = std.testing.allocator;
+    var s1 = ArrayList([]const u8).init(alloc);
+    defer s1.deinit();
+    var s2 = ArrayList([]const u8).init(alloc);
+    defer s2.deinit();
 
-  var iter1 = std.mem.split(u8, ts1, "\n");
-  var iter2 = std.mem.split(u8, ts2, "\n");
+    var iter1 = std.mem.split(u8, ts1, "\n");
+    var iter2 = std.mem.split(u8, ts2, "\n");
 
-  while (iter1.next()) |line| {
-    try s1.append(line);
-  }
-  while (iter2.next()) |line| {
-    try s2.append(line);
-  }
+    while (iter1.next()) |line| {
+        try s1.append(line);
+    }
+    while (iter2.next()) |line| {
+        try s2.append(line);
+    }
 
-  const p = try calculate_distance(alloc, s1.items, s2.items);
-  std.debug.print("Patch[0]: {s}\nPatch[1]: {s}\n", .{ p[0].to_string(alloc), p[1].to_string(alloc) });
-  const expect = std.testing.expect;
-  try expect(p.len == 2);
-  try expect(p[0].action == DiffAction.remove);
+    const p = try calculate_distance(alloc, s1.items, s2.items);
+    std.debug.print("Patch[0]: {s}\nPatch[1]: {s}\n", .{ p[0].to_string(alloc), p[1].to_string(alloc) });
+    const expect = std.testing.expect;
+    try expect(p.len == 2);
+    try expect(p[0].action == DiffAction.remove);
 }
--- src/main.zig
+++ src/main.zig
@@ -6,82 +6,81 @@ const diffing = @import("diffing.zig");
 const allocator = std.heap.page_allocator;
 
 const ArgumentState = struct {
-  then_exit: bool = false,
-  serve_mode: bool = false,
-  serve_port: u16 = 5173,
-  df_one: []const u8 = "",
-  df_two: []const u8 = "",
+    then_exit: bool = false,
+    serve_mode: bool = false,
+    serve_port: u16 = 5173,
+    df_one: []const u8 = "",
+    df_two: []const u8 = "",
 
-  pub fn print_help() void {
-    const help_string =
-      \\Usage: zeriff [command] [options]
-      \\ 
-      \\Commands:
-      \\
-      \\  diff  <file1> <file2>        Get the diff between two files, from file1 to file2.
-      \\  serve [port]                 Start the web server with a given port number (default=5173).
-      \\
-      \\General Options:
-      \\
-      \\  -h, --help                   Print this help and exit.
-      \\
-    ;
-    std.io.getStdOut().writeAll(help_string) catch {};
-  }
+    pub fn print_help() void {
+        const help_string =
+            \\Usage: zeriff [command] [options]
+            \\ 
+            \\Commands:
+            \\
+            \\  diff  <file1> <file2>        Get the diff between two files, from file1 to file2.
+            \\  serve [port]                 Start the web server with a given port number (default=5173).
+            \\
+            \\General Options:
+            \\
+            \\  -h, --help                   Print this help and exit.
+            \\
+        ;
+        std.io.getStdOut().writeAll(help_string) catch {};
+    }
 };
 
 fn parse_args(args: []const []const u8) error{NoArgumentsSupplied}!ArgumentState {
-  if (args.len == 1) {
-    ArgumentState.print_help();
-    return ArgumentState{ .then_exit = true };
-  }
-
-  const s_cmp = std.mem.eql;
-
-  var state = ArgumentState{};
-  var iter_skip: u8 = 0; // Used to skip an interation, if needed
-  var i: usize = 0;
-  while (i < args.len): (i += 1) {
-    if (iter_skip != 0) {
-      iter_skip -= 1;
-      continue;
-    }
-    const arg: []const u8 = args[i];
-    if (s_cmp(u8, arg, "-h") or s_cmp(u8, arg, "--help")) {
-      ArgumentState.print_help();
-      state.then_exit = true;
-    } else if (s_cmp(u8, arg, "diff")) {
-      if (i + 1 == args.len or i + 2 == args.len) {
-        std.io.getStdErr().writeAll("[!] Error! Not enough arguments supplied!\n") catch {};
+    if (args.len == 1) {
         ArgumentState.print_help();
-        state.then_exit = true;
-        std.os.exit(1);
-      }
-      state.df_one = args[i + 1];
-      state.df_two = args[i + 2];
-      iter_skip = 2;
-    } else if (s_cmp(u8, arg, "serve")) {
-      state.serve_mode = true;
-      // TODO: Get port, if supplied
+        return ArgumentState{ .then_exit = true };
     }
-  }
-  // TODO: Check that diff and serve mode are not both active
 
-  return state;
+    const s_cmp = std.mem.eql;
+
+    var state = ArgumentState{};
+    var iter_skip: u8 = 0; // Used to skip an interation, if needed
+    var i: usize = 0;
+    while (i < args.len) : (i += 1) {
+        if (iter_skip != 0) {
+            iter_skip -= 1;
+            continue;
+        }
+        const arg: []const u8 = args[i];
+        if (s_cmp(u8, arg, "-h") or s_cmp(u8, arg, "--help")) {
+            ArgumentState.print_help();
+            state.then_exit = true;
+        } else if (s_cmp(u8, arg, "diff")) {
+            if (i + 1 == args.len or i + 2 == args.len) {
+                std.io.getStdErr().writeAll("[!] Error! Not enough arguments supplied!\n") catch {};
+                ArgumentState.print_help();
+                state.then_exit = true;
+                std.os.exit(1);
+            }
+            state.df_one = args[i + 1];
+            state.df_two = args[i + 2];
+            iter_skip = 2;
+        } else if (s_cmp(u8, arg, "serve")) {
+            state.serve_mode = true;
+            // TODO: Get port, if supplied
+        }
+    }
+    // TODO: Check that diff and serve mode are not both active
+
+    return state;
 }
 
 pub fn main() !void {
-  var args = try std.process.argsAlloc(allocator);
-  defer std.process.argsFree(allocator, args);
-  const arg_state: ArgumentState = try parse_args(args);
-  std.debug.print("{}\n", .{ arg_state }); // TODO: Remove.
-  // TODO: Fix exit flow
-  if (arg_state.then_exit) {
-    if (args.len == 1) {
-      std.os.exit(1);
-    } else {
-      std.os.exit(0);
+    var args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+    const arg_state: ArgumentState = try parse_args(args);
+    std.debug.print("{}\n", .{arg_state}); // TODO: Remove.
+    // TODO: Fix exit flow
+    if (arg_state.then_exit) {
+        if (args.len == 1) {
+            std.os.exit(1);
+        } else {
+            std.os.exit(0);
+        }
     }
-  }
 }
-
